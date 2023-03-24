7ad9064ca66c5fdc060233965170686f20171735
shughurijr07 <abdumalikiandrew@gmail.com>
2023-03-24 22:09:00 +0300

    leave approaval approachedpayments

---
+++ database/migrations/2023_03_24_143607_leave_approvals.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+           Schema::create('leave_approvals', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('leave_id');
+            $table->string('level');
+            $table->string('done_by');
+            $table->string('comments');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('leave_approvals');
+    }
+};
---
+++ database/migrations/2023_03_24_184401_leave_entitlements.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+           Schema::create('leave_entitlements', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('staff_id');
+            $table->string('year');
+            $table->string('reason');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('leave_entitlements');
+    }
+};
